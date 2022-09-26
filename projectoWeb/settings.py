52e4cf18e862c279ba6b6531eb8779cf8d9c2bc6
Pablo Agustin Ndure <ndurepablo@gmail.com>
2022-09-26 11:52:15 -0300

    settings updated

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -16,6 +16,7 @@ import os
 from .db import DATABASES
 import dj_database_url
 
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
