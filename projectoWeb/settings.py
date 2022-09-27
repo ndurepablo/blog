1f63651864aa5f11eaeff38a04f9b42e557b6a46
Pablo Agustin Ndure <ndurepablo@gmail.com>
2022-09-27 09:50:41 -0300

    media upload to: staticfiles

--- projectoWeb/settings.py
+++ projectoWeb/settings.py
@@ -147,8 +147,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # media
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = 'static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # ckeditor
 
